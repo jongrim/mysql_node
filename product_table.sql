78a25f0254d8f8eea80d8d99891519274cc72d34
Jonathan Grim <jonjongrim@gmail.com>
2017-07-26 21:53:52 -0400

    initial commit

---
+++ product_table.sql
@@ -0,0 +1,20 @@
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Iliad', 'Classics', 14.99, 7);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('The Odyssey', 'Classics', 9.99, 11);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Romeo and Juliet', 'Shakespeare', 8.99, 30);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('The Martian', 'Science Fiction', 15.95, 22);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Cracking the Coding Interview', 'Computer Science', 34.99, 3);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Kindle', 'Electronics', 69.99, 15);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Harry Potter 7 Book Collector Set', 'Young Adult Fiction', 120.95, 1);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Calvin and Hobbes', 'Comics', 12.95, 6);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Wired, July Edition', 'Magazines', 5.99, 8);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Twilight', 'Young Adult Fiction', 14.99, 75);
